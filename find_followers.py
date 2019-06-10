987303d0b118c41de13b2e23ac8cce2fca96bde3
PatHealy <pwh5@pitt.edu>
2019-06-10 18:54:59 -0400

    grabs list of follower

--- find_followers.py
+++ find_followers.py
@@ -3,85 +3,14 @@ import sys
 import csv
 
 def show_usage():
-    print("python hashtag_search.py <hashtag> <output_file_name> [<limit>]")
-    print("\thashtag: the hashtag you're searching for")
-    print("\toutput_file_name: the name of the file this will be saved to (will be extended with .csv)")
-    print("\tlimit: the maximum number of tweets you want to query (optional). Defaults to 20. Use 'max' if you want to attempt to load without limit. Note: This limit corresponds to how many tweets are loaded but RTs are ignored regardless, so one will almost always get less than their limit of tweets.")
+    print("python find_followers.py <user_handle> <similar_profiles_file>")
+    print("\tuser_handle: the handle of the user we are attempting to find new followers for")
+    print("\toutput_file_name: the name of a file containing other profiles potential followers are likely to follow")
     print()
     print("This application also requires a configuration file named 'tweepy_config' with four lines of text representing the consumer_key, consumer_secret, access_token, and access_token_secret, respectively. For details, consult the tweepy documentation.")
 
-def clean_query_string(q):
-    q = str(q)
-    if q[0] != '#':
-        q = '#' + q
-    return '(' + q + ')'
-
-def clean_output_filename(fn):
-    fn = str(fn)
-    ind = fn.rfind('.')
-    if ind != -1:
-        fn = fn[:ind-1]
-    return fn + '.csv'
-
-def get_tweets(api, query_string, limit):
-    if limit == -1:
-        public_tweets = tweepy.Cursor(api.search, q=query_string, tweet_mode='extended').items()
-    else:
-        public_tweets = tweepy.Cursor(api.search, q=query_string, tweet_mode='extended').items(limit)
-    
-    tweets = []
-    
-    for tweet in public_tweets:
-        if 'retweeted_status' not in vars(tweet).keys():
-            t = {}
-            t['tweet_id'] = str(tweet.id)
-            t['author'] = str(tweet.author.name)
-            t['handle'] = "@" + str(tweet.author.screen_name)
-            t['created_at'] = str(tweet.created_at)
-            t['text'] = str(tweet.full_text)
-            t['media'] = []
-            print("id: " + str(tweet.id))
-            print("author: " + str(tweet.author.name))
-            print("handle: " + "@" + str(tweet.author.screen_name))
-            print("date: " + str(tweet.created_at))
-            print("text: " + str(tweet.full_text))
-            if 'extended_entities' in tweet._json and 'media' in tweet._json['extended_entities']:
-                out = 'extended media: '
-                for m in tweet._json['extended_entities']['media']:
-                    t['media'].append(str(m['media_url']))
-                    out = out + str(m['media_url']) + ', '
-                out = out[:-2]
-                print(out)
-            elif 'media' in tweet.entities:
-                out = 'media: '
-                for m in tweet.entities['media']:
-                    t['media'].append(str(m['media_url']))
-                    out = out + str(m['media_url']) + ', '
-                out = out[:-2]
-                print(out)
-            print()
-            print("====================================")
-            print()
-            tweets.append(t)
-    return tweets
-
-def save_tweets(fn, tweets):
-    with open(fn, 'w') as writeFile:
-        writer = csv.writer(writeFile)
-        
-        header = ['tweet_id', 'author', 'handle', 'datetime', 'text', 'media_link', 'media_link_2', 'media_link_3', '...']
-        writer.writerow(header)
-        
-        for tweet in tweets:
-            ln = []
-            ln.append(tweet['tweet_id'])
-            ln.append(tweet['author'])
-            ln.append(tweet['handle'])
-            ln.append(tweet['created_at'])
-            ln.append(tweet['text'])
-            for m in tweet['media']:
-                ln.append(m)
-            writer.writerow(ln)
+def get_current_followers():
+    return None
 
 try:
     consumer_key = ''
@@ -100,17 +29,33 @@ try:
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    query_string = clean_query_string(sys.argv[1])
-    output_fn = clean_output_filename(sys.argv[2])
-    limit = 20
-    if len(sys.argv) > 3:
-        if sys.argv[3] == 'max':
-            limit = -1
-        else:
-            limit = int(sys.argv[3])
+    this_handle = sys.argv[1]
+    profile = api.get_user(screen_name=this_handle)
+    if profile is None:
+        print("The given profile does not exist.")
+        raise Exception
 
-    tweets = get_tweets(api, query_string, limit)
-    save_tweets(output_fn, tweets)
+    # v = vars(profile)
+    # for k in v.keys():
+    #     print(k)
+
+    print("Name: " + profile.name)
+    print("Screen Name: " + profile.screen_name)
+    print("Followers_count: " + str(profile.followers_count))
+    print()
+
+    print("====================")
+    print("Followers")
+    print("====================")
+
+    fn = sys.argv[1] + "\\followers.csv"
+    with open(fn, 'w') as write_file:
+        writer = csv.writer(write_file)
+        writer.writerow(['Screen_name', 'Name'])
+        for follower in tweepy.Cursor(api.followers,user_id=profile.id).items():
+            print("Screen_name: " + follower.screen_name + ", Name: " + follower.name)
+            writer.writerow([follower.screen_name, follower.name])
+
 except:
-    #traceback.print_exc()
+    traceback.print_exc()
     show_usage()
