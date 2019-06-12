36b49a5e6fbfeccfd62c53c765b83bb072578f8b
PatHealy <pwh5@pitt.edu>
2019-06-12 00:23:10 -0400

    Finished get_current_followers function

--- find_followers.py
+++ find_followers.py
@@ -1,16 +1,38 @@
 import tweepy
 import sys
+import traceback
+import os
 import csv
 
 def show_usage():
     print("python find_followers.py <user_handle> <similar_profiles_file>")
     print("\tuser_handle: the handle of the user we are attempting to find new followers for")
-    print("\toutput_file_name: the name of a file containing other profiles potential followers are likely to follow")
+    print("\tsimilar_profiles_file: the name of a file containing other profiles potential followers are likely to follow")
     print()
     print("This application also requires a configuration file named 'tweepy_config' with four lines of text representing the consumer_key, consumer_secret, access_token, and access_token_secret, respectively. For details, consult the tweepy documentation.")
+    print()
 
-def get_current_followers():
-    return None
+def get_current_followers(this_handle, profile):
+    print("Name: " + profile.name)
+    print("Screen Name: " + profile.screen_name)
+    print("Followers_count: " + str(profile.followers_count))
+    print()
+
+    print("====================")
+    print("Followers")
+    print("====================")
+
+    fn = this_handle + "/followers.csv"
+    followers = []
+    with open(fn, 'w') as write_file:
+        writer = csv.writer(write_file)
+        writer.writerow(['Screen_name', 'Name', 'Bio'])
+        for follower in tweepy.Cursor(api.followers,user_id=profile.id).items():
+            print("===Screen_name: " + follower.screen_name + ",\tName: " + follower.name + '\n\t--Bio: ' + follower.description.replace('\n',' ') + '')
+            writer.writerow([follower.screen_name, follower.name, follower.description])
+            followers.append({'screen_name': follower.screen_name, 'name': follower.name, 'bio': follower.description})
+
+    return followers
 
 try:
     consumer_key = ''
@@ -35,27 +57,11 @@ try:
         print("The given profile does not exist.")
         raise Exception
 
-    # v = vars(profile)
-    # for k in v.keys():
-    #     print(k)
+    os.mkdir(this_handle)
 
-    print("Name: " + profile.name)
-    print("Screen Name: " + profile.screen_name)
-    print("Followers_count: " + str(profile.followers_count))
-    print()
-
-    print("====================")
-    print("Followers")
-    print("====================")
-
-    fn = sys.argv[1] + "\\followers.csv"
-    with open(fn, 'w') as write_file:
-        writer = csv.writer(write_file)
-        writer.writerow(['Screen_name', 'Name'])
-        for follower in tweepy.Cursor(api.followers,user_id=profile.id).items():
-            print("Screen_name: " + follower.screen_name + ", Name: " + follower.name)
-            writer.writerow([follower.screen_name, follower.name])
+    followers = get_current_followers(this_handle, profile)
 
 except:
     traceback.print_exc()
+    print("\n==========================================\n")
     show_usage()
